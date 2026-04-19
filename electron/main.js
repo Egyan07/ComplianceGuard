4f5591260bfd8aa33754c7c0d98ca3354ef88f13
Egyan07 <alexisegyan1232@gmail.com>
2026-04-20 02:45:34 +0545

    fix: use app.getAppPath() for frontend path in packaged build

--- electron/main.js
+++ electron/main.js
@@ -45,7 +45,7 @@ function createWindow() {
     mainWindow.loadURL('http://localhost:5173');
     mainWindow.webContents.openDevTools();
   } else {
-    mainWindow.loadFile(path.join(__dirname, '../frontend/dist/index.html'));
+    mainWindow.loadFile(path.join(app.getAppPath(), 'frontend', 'dist', 'index.html'));
   }
 
   mainWindow.on('closed', () => {
